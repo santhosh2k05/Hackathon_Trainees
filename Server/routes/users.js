6c7b293951c60c54e46baac899c7a0d11d0b952c
913122104307_santhoshkumar <ksdsanthosh130@gmail.com>
2025-01-12 09:54:19 +0530

    advance routing,middleware ,rendering static files

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,19 +1,44 @@
 const express = require("express")
 const router = express.Router()
+
+router.use(logger)
 router.get('/',(req,res)=>{
-    res.json({message: "error"})
+    res.send("users")
 
 })
-
 router.post('/',(req,res)=>{
     res.send("create user")
     
 })
-router.get('/:id', (req , res)=>{
-    req.params.id
-    res.send(`get user with id ${req.params.id}`)
-})
+
+
 router.get('/new',(req,res)=>{
     res.json({message : "error"})
 })
+
+
+router.route("/:id").get((req , res)=>{
+    console.log(req.user)
+    req.params.id
+    res.send(`get user with id ${req.params.id}`)
+}).put( (req , res)=>{
+    req.params.id
+    res.send(`get user with id ${req.params.id}`)
+}).delete( (req , res)=>{
+    req.params.id
+    res.send(`delete user with id ${req.params.id}`)
+})
+
+const users= [{name: "Santhosh"},{name : "teddyy"}]
+router.param("id",(req,res,next,id)=>{
+    console.log(id)
+    req.user = users[id]
+    next()
+})
+function logger(req,res ,next){
+    console.log(req.originalUrl)
+        next() 
+    
+} 
+
 module.exports = router
--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,11 @@
 const express =  require('express')
 const app = express()
 app.set("view engine", "ejs")
-app.get('/',(req ,res,next)=>{
- 
-    res.render("index", {text1: "world",text:"poda Panni"})
-   
- 
-})
+app.use(express.static( "public"))
 const userRouter = require("./routes/users")
 const postrouter = require("./routes/posts")
 app.use('/users',userRouter)
 app.use('/posts',postrouter)
+
+
 app.listen(5000)
